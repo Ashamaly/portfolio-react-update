42761d29bbc23dd01f6b344d4e43f4ba22ac6585
Ashamaly <sheriku12@gmail.com>
2020-05-20 19:05:49 +1000

    Updates

---
+++ precache-manifest.742107f6bc2d9f1a90b0af2dc789a66c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2649e541544ed50b7ff025f60ef54a",
+    "url": "/portfolio-react-update/index.html"
+  },
+  {
+    "revision": "3b0170e9de3fefa5f20a",
+    "url": "/portfolio-react-update/static/css/main.62441e16.chunk.css"
+  },
+  {
+    "revision": "b0e00a3b4d2d4bd013ba",
+    "url": "/portfolio-react-update/static/js/2.31ac59d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react-update/static/js/2.31ac59d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0170e9de3fefa5f20a",
+    "url": "/portfolio-react-update/static/js/main.5a7a5bb3.chunk.js"
+  },
+  {
+    "revision": "0a471268300290eeed38",
+    "url": "/portfolio-react-update/static/js/runtime-main.3ae58367.js"
+  },
+  {
+    "revision": "c230d2a48c0f1b39ad34733b057d651c",
+    "url": "/portfolio-react-update/static/media/chatrade.c230d2a4.png"
+  },
+  {
+    "revision": "6445dc77cfa70eaf850d7ff6bc8d5fea",
+    "url": "/portfolio-react-update/static/media/gazoot.6445dc77.png"
+  },
+  {
+    "revision": "556539b76df945b464f3896e2c709a04",
+    "url": "/portfolio-react-update/static/media/minimal1.556539b7.jpg"
+  },
+  {
+    "revision": "f43a9b817b8276982d587ccb9216e07b",
+    "url": "/portfolio-react-update/static/media/nozoma.f43a9b81.png"
+  },
+  {
+    "revision": "f3195b387fd41850228f8a75c5eb2e5b",
+    "url": "/portfolio-react-update/static/media/raw.f3195b38.png"
+  }
+]);
